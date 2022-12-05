73882a9f10431bc319aaf1e60c05e7deee691ee7
Your Namegit config --global user.email you@example.com <luisgmgomes@gmail.com>
2022-12-05 19:42:42 -0300

    organize

--- link/form/server.js
+++ link/form/server.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.post('/usuarios', (req, resp) => {
+app.post('/usuarios', (req, resp) =>
+{
     console.log(req.body)
     resp.send('<h1>Parabéns. Usuário Incluido!</h1>')
 })
 
-app.post('/usuarios/:id', (req, resp) => {
+app.post('/usuarios/:id', (req, resp) =>
+{
     console.log(req.params.id)
     console.log(req.body)
     resp.send('<h1>Parabéns. Usuário Alterado!</h1>')
